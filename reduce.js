23c7f39950735ce13a4ebe5ed84582a6d2e6d49e
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-01 12:13:22 +0600

    reduce method code write now

--- reduce.js
+++ reduce.js
@@ -22,3 +22,17 @@ console.log(re);
     return acc + curr;
  },0)
   */ 
+
+
+
+//  reduce code here:
+/**
+ * Syntax: [].reduce(callback,initilise):
+ */
+let marks = [1,2,333,45,6,56,54];
+let reduceResutl = marks.reduce(function(acc,curr){
+    return acc + curr;
+},5)
+console.log("reduceResutl here:",reduceResutl);
+
+// 0 + 1 = 1,1+2 = 3, 3 + 333 = 336,336 + 45 = ?
