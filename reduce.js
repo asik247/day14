d445799f6c3efd3a01d93eaca9afea45bc16866b
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-01 11:53:48 +0600

    reduce done

--- reduce.js
+++ reduce.js
@@ -1,1 +1,24 @@
-console.log('hi');
+let array = [1,2,3,34,45,6];
+const red = array.reduce((acc,curr)=>{
+   return acc + curr
+},0);
+console.log(red);
+
+// reduce an arry 
+let numbers = [1,2,3,33,33,56];
+const reduce = numbers.reduce(function(acc,curr){
+      return acc + curr;
+       
+},0)
+console.log(reduce);
+
+// short from code here:
+let re = numbers.reduce((acc,curr)=>acc+curr,0);
+console.log(re);
+/**
+ *Syntax reduce 
+ reduce(callbackFunction,initilise)
+ [].reduce((acc,curr){
+    return acc + curr;
+ },0)
+  */ 
